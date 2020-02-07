6a3fe7c45d7880d8698bfc6da364d258a9a7108c
Strayko <strayko_lol@windowslive.com>
2020-02-07 10:45:38 +0100

    initial commit

---
+++ src/lychrel/Lychrel.java
@@ -0,0 +1,37 @@
+package lychrel;
+
+import java.math.BigInteger;
+
+public class Lychrel {
+    public static int convergesAtIteration(int n, int limit) {
+        return converge(BigInteger.valueOf(n), 0, limit);
+    }
+
+    private static int converge(BigInteger n, int iteration, int limit){
+        if(!isPalindrome(n) && iteration < limit)
+            return converge(n.add(reverse(n)), iteration+1, limit);
+        else
+            return iteration;
+    }
+
+    static BigInteger reverse(BigInteger n) {
+        char nDigits[] = n.toString().toCharArray();
+        char rDigits[] = new char[nDigits.length];
+        int lastIndex = nDigits.length-1;
+
+        for (int i=0; i<nDigits.length; i++)
+            rDigits[i] = nDigits[lastIndex-i];
+
+        return new BigInteger(new String(rDigits));
+    }
+
+    static boolean isPalindrome(BigInteger n) {
+        String digits = n.toString();
+        int lastIndex = digits.length() - 1;
+
+        for (int i = 0; i < digits.length(); i++)
+            if (digits.charAt(i) != digits.charAt(lastIndex - i))
+                return false;
+        return true;
+    }
+}
